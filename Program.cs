f53c98d5e339e229875b88e57d5f4f72d012d99b
Murat.Can <muratt.can.94@gmail.com>
2023-02-17 23:16:13 +0300

    odev

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir Sayı Giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı = " + sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata =" + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("işlem tamamlandı");
+            }
+
+            try
+            {
+                int a =int.Parse(null);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Boş karakter girdiniz");
+            }
+
+        }
+    }
+
+}
